3656a2b8c1370c45016ab64a4480e0bc42f14c6a
Giovanni Bocci <gbocci89@gmail.com>
2021-02-15 17:31:17 -0700

    quick edit on auto.py

--- core/auto.py
+++ core/auto.py
@@ -11,7 +11,7 @@ def run_procedure(X1, X2, Y1, Y2, O1, O2):
     for mods in MultiModel:
         settings.MODEL= mods
         Y1_pred, Y2_pred, Y1_prob, Y2_prob = run_modelling_steps(X_train=X1, X_test=X2, Y_train=Y1, Y_test=Y2, model_type=settings.MODEL, nondef_params=settings.NPARA, sm=settings.SAVEMODEL, mc=settings.MULTICLASS)
-        scores = calculate_class_scores(Y1_exp=Y1, Y1_pred=Y1_pred, Y1_prob=Y1_prob, Y2_exp=Y2, Y2_pred=Y2_pred, Y2_prob=Y2_prob, O1=O1, O2=O2, mc=settings.MULTICLASS, pc=settings.PROBACUTOFF)
+        scores = calculate_class_scores(Y1_exp=Y1, Y1_pred=Y1_pred, Y1_prob=Y1_prob, Y2_exp=Y2, Y2_pred=Y2_pred, Y2_prob=Y2_prob, O1=O1, O2=O2, pc=settings.PROBACUTOFF)
         MModel_scores.append(scores)
         print(mods)
 
