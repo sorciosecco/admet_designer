fe9f14926e18473f2c1d6f660d357c3c6b753b66
Giovanni Bocci <gbocci89@gmail.com>
2021-02-26 16:17:29 -0700

    edits on dmody

--- core/regression.py
+++ core/regression.py
@@ -37,21 +37,7 @@ def run_model_training(nondef_params=False):
     
     # Random Forest (RF)
     elif settings.MODEL=="RF":
-        model = RandomForestRegressor(random_state=settings.SEED, n_jobs=-1, verbose=settings.VERBOSE
-                                      , n_estimators=100
-                                      , criterion='mse'
-                                      , max_depth=None
-                                      , max_features='auto'
-                                      , max_leaf_nodes=None
-                                      , min_samples_split=2
-                                      , min_samples_leaf=1
-                                      , min_weight_fraction_leaf=0.0
-                                      , min_impurity_decrease=0.0
-                                      , min_impurity_split=None
-                                      , bootstrap=True
-                                      , oob_score=False
-                                      , warm_start=False
-                                      )
+        model = RandomForestRegressor(random_state=settings.SEED, n_jobs=-1, verbose=settings.VERBOSE, n_estimators=100, criterion='mse', max_depth=None, max_features='auto', max_leaf_nodes=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=False, warm_start=False)
     ##### NEIGHBORS ALGORITHMS
     
     # k-Nearest Neighbors (kNN)
