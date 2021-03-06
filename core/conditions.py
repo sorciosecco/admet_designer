6a4ca280656be711a5c6f7476a6527d3b095af50
Giovanni Bocci <gbocci89@gmail.com>
2021-03-05 22:48:32 -0700

    Editing MPL-opt: layer(s) dimensions

--- core/conditions.py
+++ core/conditions.py
@@ -1,13 +1,29 @@
 
 import numpy as np
 
+#print([tuple(s-10*x for x in range(l)) for s in np.flip(np.arange(10,110,10)) for l in range(1,int(100/10)+1) if s-10*(l-1)>0])
+#print([tuple(s+10*x for x in range(l)) for s in np.arange(10,110,10) for l in range(1,int(100/10)+1) if s+10*(l-1)<100])
+# layer(s) dimensions
+increment=10
+min_layer_dim=10
+max_layer_dim=100
+#====================
+#print([tuple(s-increment*x for x in range(l)) for s in np.flip(np.arange(min_layer_dim,max_layer_dim+increment,increment)) for l in range(1,int(max_layer_dim/increment)+1) if s-increment*(l-1)>0])
+
 param_grids = {
-    'rNN': {
-        'radius': np.arange(1.0,10.1,0.1).tolist(),
-        #'weights': ['uniform', 'distance'],
-        #'leaf_size': np.arange(1,51,1).tolist(),
-        #'algorithm_knn': ['ball_tree', 'kd_tree', 'brute', 'auto'],
-        #'p': [1,2],
+    'MLP': {
+        #'solver_mlp': ['lbfgs', 'sgd', 'adam'],
+        'solver_mlp': ['adam'],
+        #'activation': ['identity', 'logistic', 'tanh', 'relu'],
+        'activation': ['logistic'],
+        #'learning_rate_init': [0.001, 0.01, 0.1, 1.0],
+        'learning_rate_init': [0.001],
+        #'learning_rate': ['constant', 'invscaling', 'adaptive'],
+        'learning_rate': ['constant'],
+        #'hidden_layer_sizes': [(s,) for s in np.arange(10,110,10)],
+        #'hidden_layer_sizes': [ tuple(10*s for _ in range(l)) for s in range(1, 10+1) for l in range(1, 5+1) ],
+        'hidden_layer_sizes': [ tuple(s-increment*x for x in range(l)) for s in np.flip(np.arange(min_layer_dim,max_layer_dim+increment,increment)) for l in range(1,int(max_layer_dim/increment)+1) if s-increment*(l-1)>0 ],
+        #'max_iter': np.arange(1,11,1).tolist(),
     },
     'kNN': {
         'n_neighbors': np.arange(3,21,1).tolist(),
@@ -54,17 +70,11 @@ param_grids = {
         'degree': [1,2,3],
         #'class_weight': [None, 'balanced'],
     },
-    'MLP': {
-        #'solver_mlp': ['lbfgs', 'sgd', 'adam'],
-        'solver_mlp': ['adam'],
-        #'activation': ['identity', 'logistic', 'tanh', 'relu'],
-        'activation': ['logistic'],
-        #'learning_rate_init': [0.001, 0.01, 0.1, 1.0],
-        'learning_rate_init': [0.001],
-        #'learning_rate': ['constant', 'invscaling', 'adaptive'],
-        'learning_rate': ['constant'],
-        #'hidden_layer_sizes': [(s,) for s in np.arange(10,110,10)],
-        'hidden_layer_sizes': [ tuple(10*s for _ in range(l)) for s in range(1, 10+1) for l in range(1, 5+1) ],
-        #'max_iter': np.arange(1,11,1).tolist(),
+    'rNN': {
+        'radius': np.arange(1.0,10.1,0.1).tolist(),
+        #'weights': ['uniform', 'distance'],
+        #'leaf_size': np.arange(1,51,1).tolist(),
+        #'algorithm_knn': ['ball_tree', 'kd_tree', 'brute', 'auto'],
+        #'p': [1,2],
     },
 }
