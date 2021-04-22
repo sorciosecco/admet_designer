784ff507623a3d5ccebf1b4ea18c80a2036049a8
Giovanni Bocci <gbocci89@gmail.com>
2021-04-22 11:18:29 -0600

    Edits on classification.py

--- core/build.py
+++ core/build.py
@@ -1,7 +1,7 @@
 
 from core import settings, variables
 from core.load import load_datasets
-from core.classification import run_modelling_steps
+from core.classification import train_the_model
 from core.regression import run_model_training
 from core.metrics import calculate_class_scores
 from core.save import save_predictions, save_multi_predictions
@@ -9,34 +9,45 @@ from core.conditions import param_grids
 from core.optimization import run_grid_cross_validation
 
 
-def run_procedure(X1, X2, Y1, Y2, O1, O2):
-    Y1_pred, Y2_pred, Y1_prob, Y2_prob = run_modelling_steps(X_train=X1, X_test=X2, Y_train=Y1, Y_test=Y2, model_type=settings.MODEL, nondef_params=settings.NPARA, sm=settings.SAVEMODEL, mc=settings.MULTICLASS)
+def print_dataset_info(V, Y1, O1, response, O2=None, Y2=None):
+    import numpy as np
+    classes, occurrences = np.unique(Y1, return_counts=True)[0].tolist(), np.unique(Y1, return_counts=True)[1].tolist()
+    message = '\nTRAINING SET\nN objects: %s\nX independent vars: %s\nY dependent vars: 1\n' % (len(O1), len(V)) + 'Y categorical (%s): %s classes => ' % (response, len(classes)) + ' + '.join(['%s (class %s)' % (occurrences[x], classes[x]) for x in range(len(classes))])
+    if O2!=None:
+        classes, occurrences = np.unique(Y2, return_counts=True)[0].tolist(), np.unique(Y2, return_counts=True)[1].tolist()
+        message += '\n\nTEST SET:\nN objects = %s\n' % (len(O2)) + 'Y (%s) = ' % (response) + ' + '.join(['%s (class %s)' % (occurrences[x], classes[x]) for x in range(len(classes))])
+    print(message)
+    return len(classes)
+
+
+#def run_procedure(X1, X2, Y1, Y2, O1, O2):
+    #Y1_pred, Y2_pred, Y1_prob, Y2_prob = run_modelling_steps(X_train=X1, X_test=X2, Y_train=Y1, Y_test=Y2, model_type=settings.MODEL, nondef_params=settings.NPARA, sm=settings.SAVEMODEL, mc=settings.MULTICLASS)
     
-    scores = calculate_class_scores(Y1_exp=Y1, Y1_pred=Y1_pred, Y1_prob=Y1_prob, Y2_exp=Y2, Y2_pred=Y2_pred, Y2_prob=Y2_prob, O1=O1, O2=O2, pc=settings.PROBACUTOFF)
+    #scores = calculate_class_scores(Y1_exp=Y1, Y1_pred=Y1_pred, Y1_prob=Y1_prob, Y2_exp=Y2, Y2_pred=Y2_pred, Y2_prob=Y2_prob, O1=O1, O2=O2, pc=settings.PROBACUTOFF)
     
-    if settings.SAVEPRED:
-        if settings.MULTICLASS: save_multi_predictions(name=settings.MODEL, Y1_exp=Y1, Y1_pred=Y1_pred, Y1_prob=Y1_prob, Y2_exp=Y2, Y2_pred=Y2_pred, Y2_prob=Y2_prob, O1=O1, O2=O2)
-        else: save_predictions(name=settings.MODEL, Y1_exp=Y1, Y1_pred=Y1_pred, Y1_prob=Y1_prob, Y2_exp=Y2, Y2_pred=Y2_pred, Y2_prob=Y2_prob, O1=O1, O2=O2, pc=settings.PROBACUTOFF)
-    else:
-        if settings.VERBOSE!=0: print("\n*** Predicted values have not been saved.\n")
+    #if settings.SAVEPRED:
+        #if settings.MULTICLASS: save_multi_predictions(name=settings.MODEL, Y1_exp=Y1, Y1_pred=Y1_pred, Y1_prob=Y1_prob, Y2_exp=Y2, Y2_pred=Y2_pred, Y2_prob=Y2_prob, O1=O1, O2=O2)
+        #else: save_predictions(name=settings.MODEL, Y1_exp=Y1, Y1_pred=Y1_pred, Y1_prob=Y1_prob, Y2_exp=Y2, Y2_pred=Y2_pred, Y2_prob=Y2_prob, O1=O1, O2=O2, pc=settings.PROBACUTOFF)
+    #else:
+        #if settings.VERBOSE!=0: print("\n*** Predicted values have not been saved.\n")
     
 
 def build_classification_model(args):
-    settings.NPARA=args.npara
-    settings.MULTICLASS=args.multiclass
-    settings.PROBACUTOFF=args.probacutoff
-    settings.SAVEMODEL=args.savemodel
-    settings.SAVEPRED=args.savepred
-    settings.GRIDSEARCH=args.gridsearch
-    #settings.BACKFEEL=args.backfeel
-    
-    X1, X2, Y1, Y2, O1, O2, V = load_datasets(training=settings.FIT, test=settings.PREDICT, response=settings.RESPONSE)
-    settings.VAR_NAMES=V[1:-1]
-    settings.VARS=V
-    
+    settings.NPARA, settings.OPTIMIZE = args.npara, args.optimize
+    #settings.PROBACUTOFF=args.probacutoff
+    #settings.SAVEMODEL=args.savemodel
+    #settings.SAVEPRED=args.savepred
+    if settings.PREDICT==None:
+    #X1, X2, Y1, Y2, O1, O2, V = load_datasets(training=settings.FIT, test=settings.PREDICT, response=settings.RESPONSE)
+    #settings.VAR_NAMES=V[1:-1]
+    #settings.VARS=V
+        variables.X_tra, variables.Y_tra, variables.O_list, variables.V_list = load_datasets(training=settings.FIT, response=settings.RESPONSE)
+        
+        classes = print_dataset_info(V=variables.V_list, Y1=variables.Y_tra, O1=variables.O_list, response=settings.RESPONSE)
     #if settings.GRIDSEARCH: gridsearchcv(X1=X1, Y1=Y1, X2=X2, Y2=Y2, grid=param_grids[settings.MODEL])
     #else: run_procedure(X1=X1, X2=X2, Y1=Y1, Y2=Y2, O1=O1, O2=O2)
-    run_procedure(X1=X1, X2=X2, Y1=Y1, Y2=Y2, O1=O1, O2=O2)
+    #run_procedure(X1=X1, X2=X2, Y1=Y1, Y2=Y2, O1=O1, O2=O2)
+        train_the_model()
     
 
 def build_regression_model(args):
--- core/classification.py
+++ core/classification.py
@@ -1,217 +1,53 @@
-# NB: rNN has no probabilities!!!
+##### NB: rNN has no probabilities!!!
 
-from core import settings
-from core import parameters
+from core import settings, parameters
 from core.save import save_model, save_vars_importance
-#from core.descriptors import backward_feature_elimination
 
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier, ExtraTreesClassifier
 from sklearn.svm import SVC
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier#, RadiusNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
-
 from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
 
-def algorithm_setup(model_type, nondef_params):
-    ### TREE ENSEMBLE ALGORITHMS
-    
-    # Random Forest (RF)
-    if model_type=="RF":
-        if nondef_params: model=RandomForestClassifier(random_state=settings.SEED
-                                                       , n_estimators=parameters.n_estimators
-                                                       , class_weight=parameters.class_weight
-                                                       , criterion=parameters.criterion_rf
-                                                       , max_depth=parameters.max_depth
-                                                       , max_features=parameters.max_features
-                                                       , max_leaf_nodes=parameters.max_leaf_nodes
-                                                       )
-        else: model=RandomForestClassifier(random_state=settings.SEED
-                                           , n_estimators=100
-                                           , class_weight=None
-                                           , criterion='gini'
-                                           , max_depth=None
-                                           , max_features='auto'
-                                           , max_leaf_nodes=None
-                                           , min_samples_split=2
-                                           , min_samples_leaf=1
-                                           , min_weight_fraction_leaf=0.0
-                                           , min_impurity_decrease=0.0
-                                           , min_impurity_split=None
-                                           , bootstrap=True
-                                           , oob_score=False
-                                           , warm_start=False
-                                           )
-    # Ada Boost (AB)
-    if model_type=="AB":
-        if nondef_params: model=AdaBoostClassifier(random_state=settings.SEED, base_estimator=DecisionTreeClassifier(max_depth=1)
-                                                   , n_estimators=parameters.n_estimators
-                                                   , algorithm=parameters.algorithm_ab
-                                                   )
-        else: model=AdaBoostClassifier(random_state=settings.SEED, base_estimator=DecisionTreeClassifier(max_depth=1)
-                                       , n_estimators=100
-                                       , algorithm='SAMME.R'
-                                       , learning_rate=1.0
-                                       )
-    # Extra Trees Classifier (ETC)
-    if model_type=="ETC":
-        if nondef_params: model=ExtraTreesClassifier(random_state=settings.SEED, bootstrap=True
-                                                     , n_estimators=parameters.n_estimators
-                                                     , class_weight=parameters.class_weight
-                                                     , criterion=parameters.criterion_rf
-                                                     , max_depth=parameters.max_depth
-                                                     , max_features=parameters.max_features
-                                                     , max_leaf_nodes=parameters.max_leaf_nodes
-                                                     )
-        else: model=ExtraTreesClassifier(random_state=settings.SEED, bootstrap=True
-                                         , n_estimators=100
-                                         , class_weight=None
-                                         , criterion='gini'
-                                         , max_depth=None
-                                         , max_features='auto'
-                                         , max_leaf_nodes=None
-                                         , min_samples_split=2
-                                         , min_samples_leaf=1
-                                         , min_weight_fraction_leaf=0.0
-                                         , min_impurity_decrease=0.0
-                                         , min_impurity_split=None
-                                         , oob_score=False
-                                         , warm_start=False
-                                         )
-    # Gradient Boosting (GB)
-    if model_type=="GB":
-        if nondef_params: model=GradientBoostingClassifier(random_state=settings.SEED
-                                                           , n_estimators=parameters.n_estimators
-                                                           , max_depth=parameters.max_depth
-                                                           , max_features=parameters.max_features
-                                                           , max_leaf_nodes=parameters.max_leaf_nodes
-                                                           , loss=parameters.loss
-                                                           , criterion=parameters.criterion_gb
-                                                           )
-        else: model=GradientBoostingClassifier(random_state=settings.SEED
-                                               , n_estimators=100
-                                               , max_depth=3
-                                               , max_features=None
-                                               , max_leaf_nodes=None
-                                               , learning_rate=0.1
-                                               , loss='deviance'
-                                               , subsample=1.0
-                                               , criterion='friedman_mse'
-                                               , min_samples_split=2
-                                               , min_samples_leaf=1
-                                               , min_weight_fraction_leaf=0.0
-                                               , min_impurity_decrease=0.0
-                                               , min_impurity_split=None
-                                               , init=None
-                                               , warm_start=False
-                                               , validation_fraction=0.1
-                                               , n_iter_no_change=None
-                                               , tol=0.0001
-                                               )
-    
-    ### NEAREST NEIGHBORS ALGORITMS
-    
-    # k-Nearest Neighbors (kNN)
-    if model_type=="kNN":
-        if nondef_params: model=KNeighborsClassifier(n_neighbors=parameters.n_neighbors
-                                                     #, leaf_size=parameters.leaf_size
-                                                     #, algorithm=parameters.algorithm_knn
-                                                     , p=parameters.p
-                                                     , weights=parameters.weights
-                                                     )
-        else: model=KNeighborsClassifier(n_neighbors=5
-                                         , weights='uniform'
-                                         , algorithm='auto'
-                                         , leaf_size=30
-                                         , p=2
-                                         , metric='minkowski'
-                                         , metric_params=None
-                                         )
-    # radious Nearest Neighbors (rNN)
-    if model_type=="rNN":
-        if nondef_params: model=RadiusNeighborsClassifier(radius=parameters.radius, p=parameters.p, weights=parameters.weights)
-        else: model=RadiusNeighborsClassifier(radius=1.0
-                                              , weights='uniform'
-                                              , algorithm='auto'
-                                              , leaf_size=30
-                                              , p=2
-                                              , metric='minkowski'
-                                              , metric_params=None
-                                              )
-    
-    ### LINEAR ALGORITHMS
-    
-    # Linear Discriminant Analysis (LDA)
-    if model_type=="LDA":
-        if nondef_params: model=LinearDiscriminantAnalysis(solver=parameters.solver
-                                                           , shrinkage=parameters.shrinkage
-                                                           )
-        else: model=LinearDiscriminantAnalysis(solver='lsqr'
-                                               , shrinkage='auto'
-                                               , priors=None
-                                               , n_components=None
-                                               , store_covariance=False
-                                               , tol=0.0001
-                                               )
-    
-    ### NEURAL NETWORKS
-    
-    # Multi-layer Perception (MLP)
-    if model_type=="MLP":
-        if nondef_params: model=MLPClassifier(random_state=settings.SEED
-                                              , hidden_layer_sizes=parameters.hidden_layer_sizes
-                                              , max_iter=parameters.max_iter
-                                              )
-        else: model=MLPClassifier(random_state=settings.SEED
-                                  , hidden_layer_sizes=(100,)
-                                  , activation='relu'
-                                  , solver='lbfgs'
-                                  , alpha=0.0001
-                                  , batch_size='auto'
-                                  , learning_rate='constant'
-                                  , learning_rate_init=0.001
-                                  , power_t=0.5
-                                  , shuffle=True
-                                  , tol=0.0001
-                                  , warm_start=False
-                                  , momentum=0.9
-                                  , nesterovs_momentum=True
-                                  , early_stopping=False
-                                  , validation_fraction=0.1
-                                  , beta_1=0.9
-                                  , beta_2=0.999
-                                  , epsilon=1e-08
-                                  , n_iter_no_change=10
-                                  , max_iter=500
-                                  )
-    
-    ### OTHER ALGORITHMS
-    
-    # Support Vector Machines (SVM)
-    if model_type=="SVM":
-        if nondef_params: model=SVC(probability=True, random_state=settings.SEED
-                                    , C=parameters.C
-                                    , degree=parameters.degree
-                                    , gamma=parameters.gamma
-                                    , kernel=parameters.kernel
-                                    , class_weight=parameters.class_weight
-                                    )
-        else: model=SVC(random_state=settings.SEED, probability=True
-                        , C=1.0
-                        , kernel='rbf'
-                        , degree=3
-                        , gamma='auto'
-                        , coef0=0.0
-                        , shrinking=True
-                        , tol=0.001
-                        , cache_size=200
-                        , class_weight=None
-                        , decision_function_shape='ovr'
-                        )
-    return model
 
-
+def train_the_model():
+    Xe, Ye = variables.X_tra, variables.Y_tra
+    
+    if settings.NPARA==False: ##### ALGORITHMS WITH DEFAULT PARAMETERS
+        if settings.MODEL=="RF": model=RandomForestClassifier(random_state=settings.SEED, n_estimators=100, class_weight=None, criterion='gini', max_depth=None, max_features='auto', max_leaf_nodes=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=False, warm_start=False)
+        elif settings.MODEL=="AB": model=AdaBoostClassifier(random_state=settings.SEED, base_estimator=None, n_estimators=100, algorithm='SAMME.R', learning_rate=1.0)
+        elif settings.MODEL=="ET": model=ExtraTreesClassifier(random_state=settings.SEED, bootstrap=True, n_estimators=100, class_weight=None, criterion='gini', max_depth=None, max_features='auto', max_leaf_nodes=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, min_impurity_decrease=0.0, min_impurity_split=None, oob_score=False, warm_start=False)
+        elif settings.MODEL=="GB": model=GradientBoostingClassifier(random_state=settings.SEED, n_estimators=100, max_depth=3, max_features=None, max_leaf_nodes=None, learning_rate=0.1, loss='deviance', subsample=1.0, criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, min_impurity_decrease=0.0, min_impurity_split=None, init=None, warm_start=False, validation_fraction=0.1, n_iter_no_change=None, tol=0.0001)
+        elif settings.MODEL=="kNN": model=KNeighborsClassifier(n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None)
+        #elif settings.MODEL=="rNN": model=RadiusNeighborsClassifier(radius=1.0, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None)
+        elif settings.MODEL=="LDA": model=LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto', priors=None, n_components=None, store_covariance=False, tol=0.0001)
+        elif settings.MODEL=="MLP": model=MLPClassifier(random_state=settings.SEED, hidden_layer_sizes=(100,), activation='relu', solver='lbfgs', alpha=0.0001, batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, shuffle=True, tol=0.0001, warm_start=False, momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, n_iter_no_change=10, max_iter=500)
+        elif settings.MODEL=="SVM": model=SVC(random_state=settings.SEED, probability=True, C=1.0, kernel='rbf', degree=3, gamma='auto', coef0=0.0, shrinking=True, tol=0.001, cache_size=200, class_weight=None, decision_function_shape='ovr')
+        else: print("\nERROR: algorithm not supported\n")
+        
+    else:
+        if settings.MODEL=="RF": model=RandomForestClassifier(random_state=settings.SEED, n_estimators=parameters.n_estimators, class_weight=parameters.class_weight, criterion=parameters.criterion_rf, max_depth=parameters.max_depth, max_features=parameters.max_features, max_leaf_nodes=parameters.max_leaf_nodes)
+        elif settings.MODEL=="AB": model=AdaBoostClassifier(random_state=settings.SEED, n_estimators=parameters.n_estimators, algorithm=parameters.algorithm_ab)
+        elif settings.MODEL=="ET": model=ExtraTreesClassifier(random_state=settings.SEED, bootstrap=True, n_estimators=parameters.n_estimators, class_weight=parameters.class_weight, criterion=parameters.criterion_rf, max_depth=parameters.max_depth, max_features=parameters.max_features, max_leaf_nodes=parameters.max_leaf_nodes)
+        elif settings.MODEL=="GB": model=GradientBoostingClassifier(random_state=settings.SEED, n_estimators=parameters.n_estimators, max_depth=parameters.max_depth, max_features=parameters.max_features, max_leaf_nodes=parameters.max_leaf_nodes, loss=parameters.loss, criterion=parameters.criterion_gb)
+        elif settings.MODEL=="kNN": model=KNeighborsClassifier(n_neighbors=parameters.n_neighbors, leaf_size=parameters.leaf_size, algorithm=parameters.algorithm_knn, p=parameters.p, weights=parameters.weights)
+        #elif settings.MODEL=="rNN": model=RadiusNeighborsClassifier(radius=parameters.radius, p=parameters.p, weights=parameters.weights)
+        elif settings.MODEL=="LDA": model=LinearDiscriminantAnalysis(solver=parameters.solver, shrinkage=parameters.shrinkage)
+        elif settings.MODEL=="MLP": model=MLPClassifier(random_state=settings.SEED, hidden_layer_sizes=parameters.hidden_layer_sizes, max_iter=parameters.max_iter)
+        elif settings.MODEL=="SVM": model=SVC(random_state=settings.SEED, C=parameters.C, degree=parameters.degree, gamma=parameters.gamma, kernel=parameters.kernel, class_weight=parameters.class_weight)
+        else: print("\nERROR: algorithm not supported\n")
+        
+    if settings.MODEL!="PLS":
+        variables.model = model
+        if variables.DMODY:
+            q2, sdep, variables.Y_pred = leave_one_out(X=np.array(Xe), Y=np.array(Ye), M=model)
+            print("Q2\tSDEP\n%s\t%s" % (round(q2,3), round(sdep,3)))
+        if settings.OPTIMIZE==False:
+            model.fit(Xe, Ye)
+            scores = cross_validation(X=Xe, Y=Ye, M=model)
+            print("cross-validation results:\nCV1\tCV2\tCV3\tCV4\tCV5\tAVER\n%s\t%s\t%s\t%s\t%s\t%s" % tuple([round(s,3) for s in scores] + [round(np.mean(scores),3)]))
+            
 def define_model_for_optimization(mt, ndp, mc):
     model=algorithm_setup(model_type=mt, nondef_params=ndp)
     if mc:
